bf12d0cffa81175172da3ac1e5e84a611d64f729
yaroslav-volkov-dev <volkov.yaroslav94@gmail.com>
2025-05-13 18:44:30 +0300

    initial commit, created UsersService, implemented filter, map, foreach methods

--- Mentoring.Players.Repository/Program.cs
+++ Mentoring.Players.Repository/Program.cs
@@ -1,11 +1,33 @@
 ﻿
+using Mentoring.Players.Repository.Models;
+using Mentoring.Players.Repository.Services;
+
 namespace Mentoring.Players.Repository;
 
 public static class Program
 {
+    private static List<Player> mockPlayers = new()
+    {
+        new Player("Yaroslav", 10),
+        new Player("Dmitriy", 30)
+    };
+    
+    private static PlayersService _playersService = new();
+    
     private static void Main(string[] args)
     {
-        ConsoleInterface consoleInterface = new();
-        consoleInterface.Run();
+        // ConsoleInterface consoleInterface = new();
+        // consoleInterface.Run();
+        
+        List<Player> filteredPlayers = _playersService.Filter(mockPlayers, (player) => player.Level > 10);
+        
+        List<Player> mappedPlayers = _playersService.Map(mockPlayers, (player) =>
+        {
+            player.IsBanned = true;
+            return player;
+        });
+        
+        _playersService.ForEach(mappedPlayers, Console.WriteLine);
+        _playersService.ForEach(filteredPlayers, Console.WriteLine);
     }
 }
